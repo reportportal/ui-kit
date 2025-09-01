94f4d93bd0b2791b29658da46d3c581df548a2e7
blackymax <maxmin3007@gmail.com>
2025-09-01 08:04:25 +0300

    fix: single autocomplete

--- src/components/autocompletes/singleAutocomplete/singleAutocomplete.tsx
+++ src/components/autocompletes/singleAutocomplete/singleAutocomplete.tsx
@@ -14,15 +14,12 @@
  * limitations under the License.
  */
 
-import React, { ChangeEvent, Component, ComponentProps, KeyboardEvent, Ref } from 'react';
+import { ChangeEvent, ComponentProps, KeyboardEvent, Ref } from 'react';
 import Downshift, { DownshiftState, StateChangeOptions } from 'downshift';
-import PropTypes from 'prop-types';
-import { Manager, Reference, Popper } from 'react-popper';
-// import { FieldText } from 'componentLibrary/fieldText';
 import { AutocompleteMenu } from '../common/autocompleteMenu';
-import { autocompleteVariantType, singleAutocompleteOptionVariantType } from '../common/propTypes';
 import FieldText from '@/components/fieldText';
 import { ENTER_KEY_NAME, TAB_KEY_NAME } from '../constants';
+import { useFloating } from '@floating-ui/react';
 
 const DEFAULT_OPTIONS_INDEX = 0;
 
@@ -60,38 +57,42 @@ export interface SingleAutocompleteProps<T, K> {
   useFixedPositioning: boolean;
 }
 
-export class SingleAutocomplete<T, K> extends Component<SingleAutocompleteProps<T, K>> {
-  static defaultProps = {
-    options: [],
-    loading: false,
-    value: '',
-    placeholder: '',
-    onChange: () => {},
-    onFocus: () => {},
-    onBlur: () => {},
-    disabled: false,
-    inputProps: {},
-    parseValueToString: (value) => value || '',
-    renderOption: null,
-    minLength: 1,
-    maxLength: null,
-    async: false,
-    optionVariant: '',
-    isRequired: false,
-    error: '',
-    touched: false,
-    setTouch: () => {},
-    createWithoutConfirmation: false,
-    menuClassName: '',
-    icon: null,
-    isOptionUnique: null,
-    refFunction: () => {},
-    stateReducer: (state, changes) => changes,
-    variant: 'light',
-    useFixedPositioning: false,
-  };
+export const SingleAutocomplete = <T, K>(componentProps: SingleAutocompleteProps<T, K>) => {
+  const {
+    options = [],
+    value,
+    placeholder = '',
+    onChange = () => {},
+    onFocus = () => {},
+    onBlur = () => {},
+    disabled = false,
+    inputProps = {},
+    parseValueToString = (value) => value || '',
+    renderOption = null,
+    minLength = 1,
+    maxLength = null,
+    optionVariant = '',
+    isRequired = false,
+    error = '',
+    touched = false,
+    setTouch = () => {},
+    createWithoutConfirmation = false,
+    menuClassName = '',
+    icon = null,
+    isOptionUnique = null,
+    refFunction = () => {},
+    stateReducer,
+    variant = 'light',
+    useFixedPositioning = false,
+    onStateChange,
+    ...props
+  } = componentProps;
 
-  getOptionProps =
+  const { refs, floatingStyles } = useFloating({
+    placement: 'bottom-start',
+  });
+
+  const getOptionProps =
     (getItemProps, highlightedIndex, selectedItem) =>
     ({ item, index, ...rest }) =>
       getItemProps({
@@ -102,144 +103,96 @@ export class SingleAutocomplete<T, K> extends Component<SingleAutocompleteProps<
         ...rest,
       });
 
-  handleKeyDown = (
+  const handleKeyDown = (
     event: KeyboardEvent<HTMLInputElement>,
     setHighlightedIndex: (value: number) => void,
   ) => {
     if (event.key === TAB_KEY_NAME) {
       event.preventDefault();
-      setHighlightedIndex(this.props.options.length);
+      setHighlightedIndex(options.length);
     }
   };
 
-  render() {
-    const {
-      onStateChange,
-      onChange,
-      onBlur,
-      onFocus,
-      parseValueToString,
-      placeholder,
-      disabled,
-      value,
-      inputProps,
-      maxLength,
-      optionVariant,
-      isRequired,
-      error,
-      touched,
-      setTouch,
-      createWithoutConfirmation,
-      menuClassName,
-      icon,
-      options,
-      isOptionUnique,
-      refFunction,
-      stateReducer,
-      variant,
-      useFixedPositioning,
-      ...props
-    } = this.props;
-    return (
-      <Manager>
-        <Downshift
-          onChange={onChange}
-          itemToString={parseValueToString}
-          selectedItem={value}
-          onStateChange={onStateChange}
-          defaultHighlightedIndex={DEFAULT_OPTIONS_INDEX}
-          stateReducer={stateReducer}
-        >
-          {({
-            getInputProps,
-            getItemProps,
-            setHighlightedIndex,
-            isOpen,
-            inputValue,
-            highlightedIndex,
-            selectItem,
-          }) => (
-            <div>
-              <Reference>
-                {({ ref }) => (
-                  <div ref={ref}>
-                    <FieldText
-                      {...getInputProps({
-                        placeholder: !disabled ? placeholder : '',
-                        maxLength,
-                        onFocus: () => {
-                          onFocus();
-                        },
-                        refFunction,
-                        onKeyDown: (event) => {
-                          if (event.key === ENTER_KEY_NAME) {
-                            event.preventDefault();
-                          }
+  return (
+    <Downshift
+      onChange={onChange}
+      itemToString={parseValueToString}
+      selectedItem={value}
+      onStateChange={onStateChange}
+      defaultHighlightedIndex={DEFAULT_OPTIONS_INDEX}
+      stateReducer={stateReducer}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        setHighlightedIndex,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+        selectItem,
+      }) => (
+        <>
+          <div ref={refs.setReference}>
+            <FieldText
+              {...getInputProps({
+                placeholder: !disabled ? placeholder : '',
+                maxLength,
+                onFocus: () => {
+                  onFocus();
+                },
+                refFunction,
+                onKeyDown: (event) => {
+                  if (event.key === ENTER_KEY_NAME) {
+                    event.preventDefault();
+                  }
 
-                          if (inputValue && isOpen) {
-                            this.handleKeyDown(event, setHighlightedIndex);
-                          }
-                        },
-                        onBlur: (e) => {
-                          const newValue = (inputValue || '').trim() as T;
+                  if (inputValue && isOpen) {
+                    handleKeyDown(event, setHighlightedIndex);
+                  }
+                },
+                onBlur: (e) => {
+                  const newValue = (inputValue || '').trim() as T;
 
-                          if (!createWithoutConfirmation && !newValue) {
-                            selectItem(newValue);
-                          }
+                  if (!createWithoutConfirmation && !newValue) {
+                    selectItem(newValue);
+                  }
 
-                          if (createWithoutConfirmation) {
-                            selectItem(newValue);
-                          }
+                  if (createWithoutConfirmation) {
+                    selectItem(newValue);
+                  }
 
-                          onBlur(e);
-                          isOptionUnique?.(newValue ? !options.find((v) => v === newValue) : null);
-                          setTouch(true);
-                        },
-                        disabled,
-                        defaultWidth: false,
-                        isRequired,
-                        touched,
-                        error,
-                        endIcon: icon,
-                        variant,
-                        ...inputProps,
-                      })}
-                    />
-                  </div>
-                )}
-              </Reference>
-              <Popper
-                placement="bottom-start"
-                positionFixed={useFixedPositioning}
-                modifiers={{
-                  preventOverflow: { escapeWithReference: true },
-                  flip: {
-                    enabled: true,
-                  },
-                }}
-              >
-                {({ placement, ref, style }) => (
-                  <AutocompleteMenu
-                    isOpen={isOpen}
-                    placement={placement}
-                    style={style}
-                    ref={ref}
-                    inputValue={(inputValue || '').trim()}
-                    getItemProps={this.getOptionProps(getItemProps, highlightedIndex, value)}
-                    parseValueToString={parseValueToString}
-                    optionVariant={optionVariant}
-                    createWithoutConfirmation={createWithoutConfirmation}
-                    className={menuClassName}
-                    options={options}
-                    variant={variant}
-                    {...props}
-                  />
-                )}
-              </Popper>
-            </div>
-          )}
-        </Downshift>
-      </Manager>
-    );
-  }
-}
+                  onBlur(e);
+                  isOptionUnique?.(newValue ? !options.find((v) => v === newValue) : null);
+                  setTouch(true);
+                },
+                disabled,
+                defaultWidth: false,
+                isRequired,
+                touched,
+                error,
+                endIcon: icon,
+                variant,
+                ...inputProps,
+              })}
+            />
+          </div>
+
+          <AutocompleteMenu
+            isOpen={isOpen}
+            style={floatingStyles}
+            ref={refs.setFloating}
+            inputValue={(inputValue || '').trim()}
+            getItemProps={getOptionProps(getItemProps, highlightedIndex, value)}
+            parseValueToString={parseValueToString}
+            optionVariant={optionVariant}
+            createWithoutConfirmation={createWithoutConfirmation}
+            className={menuClassName}
+            options={options}
+            variant={variant}
+            {...props}
+          />
+        </>
+      )}
+    </Downshift>
+  );
+};
