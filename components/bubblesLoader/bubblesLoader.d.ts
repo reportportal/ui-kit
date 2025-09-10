3957cf3c314fc5e9c278bd69a43c3861495784dd
yelyzavetakhokhlova <yelyzavetakhokhlova@users.noreply.github.com>
2025-09-10 12:29:08 +0000

    Add large variant for bubbles loader (#136) 005a0c5afb3a58c11b1cb2b94ad22bacf6313913

--- components/bubblesLoader/bubblesLoader.d.ts
+++ components/bubblesLoader/bubblesLoader.d.ts
@@ -3,6 +3,7 @@ import { FC } from '../../../node_modules/react';
 interface BubblesLoaderProps {
     color?: string;
     className?: string;
+    variant?: 'standard' | 'large';
 }
 export declare const BubblesLoader: FC<BubblesLoaderProps>;
 export {};
