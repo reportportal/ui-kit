f6b34ff982969d1e56e9c1f61f723a0941ae49e1
yelyzavetakhokhlova <yelyzavetakhokhlova@users.noreply.github.com>
2025-09-11 12:22:24 +0000

    Fix attached file name truncation (#138) c659462fb29adc92ed771cdf1bc4d54ce5937b09

--- components/fileDropArea/index.d.ts
+++ components/fileDropArea/index.d.ts
@@ -1,2 +1,6 @@
-export { FileDropArea } from './fileDropArea';
-export { MIME_TYPES } from './types';
+import { FileDropArea } from './fileDropArea';
+import { MIME_TYPES, FileWithValidation } from './types';
+
+export { FileDropArea };
+export { MIME_TYPES };
+export type { FileWithValidation };
