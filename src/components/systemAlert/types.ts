8d0679a7cf09bbc068e2aae951e038a59022cb3f
Oleksandr Dragun <oleksandr_dragun@epam.com>
2025-09-14 17:10:49 +0300

    add warning state to system alert

--- src/components/systemAlert/types.ts
+++ src/components/systemAlert/types.ts
@@ -2,13 +2,20 @@ import { ReactElement } from 'react';
 export enum SystemAlertType {
   INFO = 'info',
   SUCCESS = 'success',
+  WARNING = 'warning',
   ERROR = 'error',
+}
+
+export enum SystemAlertTypographyColorType {
+  WHITE = 'white',
+  BLACK = 'black',
 }
 export interface SystemAlertProps {
   title: string;
   onClose: () => void;
   icon?: ReactElement | null;
   type?: SystemAlertType;
+  typographyColor?: SystemAlertTypographyColorType;
   duration?: number;
   className?: string;
   dataAutomationId?: string;
