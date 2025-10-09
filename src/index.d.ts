79ea16d074690187b9f0c935d875bb1a049ccd79
mcharnou <mcharnou@adnoc.ae>
2025-10-09 08:48:47 +0300

    fix: issues

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,6 @@
-import React from 'react';
-
+import * as React from 'react';
 declare module 'react' {
-  function forwardRef<T, P = {}>(
-    render: (props: P, ref: ForwardedRef<T>) => ReactElement | null,
-  ): (props: P & RefAttributes<T>) => ReactElement | null;
+  function forwardRef<T, P = object>(
+    render: (props: P, ref: React.ForwardedRef<T>) => React.ReactElement | null,
+  ): (props: P & React.RefAttributes<T>) => React.ReactElement | null;
 }
