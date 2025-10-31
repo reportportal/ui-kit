d8d3393e2b7a1da06d525006377eca8e96ceff68
yelyzavetakhokhlova <yelyzavetakhokhlova@users.noreply.github.com>
2025-10-31 15:48:57 +0000

    [BREAKING] Update package exports (#155) ff347caae4af28e3e10e81cfd1cc809b55e1badc

--- components/modal/index.d.ts
+++ components/modal/index.d.ts
@@ -2,3 +2,4 @@ export { Modal } from './modal';
 export { ModalContent } from './modalContent';
 export { ModalHeader } from './modalHeader';
 export { ModalFooter } from './modalFooter';
+export type { ExtendedButtonProps } from './types';
--- components/table/index.d.ts
+++ components/table/index.d.ts
@@ -1,4 +1,5 @@
 import { Table } from './table';
 
 export { Table };
+export type { RowData, Column, PrimaryColumn, FixedColumn, PinnedColumn, RowConfigs, DetailedCellData, MetaData, SortConfig, SortingDirection, TableComponentProps, } from './types';
 export default Table;
