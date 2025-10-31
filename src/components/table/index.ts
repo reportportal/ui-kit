ff347caae4af28e3e10e81cfd1cc809b55e1badc
Yelyzaveta Khokhlova <69502158+yelyzavetakhokhlova@users.noreply.github.com>
2025-10-31 17:47:55 +0200

    [BREAKING] Update package exports (#155)

--- src/components/table/index.ts
+++ src/components/table/index.ts
@@ -1,5 +1,18 @@
 import { Table } from './table';
 
 export { Table };
+export type {
+  RowData,
+  Column,
+  PrimaryColumn,
+  FixedColumn,
+  PinnedColumn,
+  RowConfigs,
+  DetailedCellData,
+  MetaData,
+  SortConfig,
+  SortingDirection,
+  TableComponentProps,
+} from './types';
 
 export default Table;
