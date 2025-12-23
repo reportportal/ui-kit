1309e432547b6192eac1bf9f6eb636f86b6a8120
siarheirazuvalau <siarheirazuvalau@users.noreply.github.com>
2025-12-23 06:52:11 +0000

    EPMRPP-110986 || Fix incorrect cell height calculation and gradient sizes on expand all/collapse all (#204)
    EPMRPP-110986 || Fix incorrect cell height calculation and gradient sizes on expand all/collapse all 151e65c81a582514f9ec628dd128ecd97158d9f2

--- components/table/hooks/useGradientPosition.d.ts
+++ components/table/hooks/useGradientPosition.d.ts
@@ -20,5 +20,5 @@ export interface PinnedGradientPosition {
         height: number;
     };
 }
-export declare const useRightGradientPosition: (table: HTMLElement | null, header: HTMLElement | null, scrollContainer: HTMLElement | null, isHeaderPinned: boolean, scrollLeft: number, scrollTop: number, tableScrollWidth: number, windowResizeCounter: number) => RightGradientPosition;
-export declare const usePinnedGradientPosition: (table: HTMLElement | null, scrollLeft: number, header: HTMLElement | null, scrollContainer: HTMLElement | null, isHeaderPinned: boolean, scrollTop: number, tableScrollWidth: number, windowResizeCounter: number) => PinnedGradientPosition;
+export declare const useRightGradientPosition: (table: HTMLElement | null, header: HTMLElement | null, scrollContainer: HTMLElement | null, isHeaderPinned: boolean, scrollLeft: number, scrollTop: number, tableScrollWidth: number, gradientUpdateCounter: number) => RightGradientPosition;
+export declare const usePinnedGradientPosition: (table: HTMLElement | null, scrollLeft: number, header: HTMLElement | null, scrollContainer: HTMLElement | null, isHeaderPinned: boolean, scrollTop: number, tableScrollWidth: number, gradientUpdateCounter: number) => PinnedGradientPosition;
