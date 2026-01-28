733684fcace42727fb06cf99a0f4a7910cb98e47
siarheirazuvalau <siarhei_razuvalau@epam.com>
2026-01-28 11:05:09 +0300

    EPMRPP-111251 || Add SegmentedControl component (#222)

--- src/components/index.ts
+++ src/components/index.ts
@@ -26,6 +26,7 @@ export { MultipleAutocomplete } from './autocompletes/multipleAutocomplete';
 export { Pagination } from './pagination';
 export { Popover } from './popover';
 export { Radio } from './radio';
+export { SegmentedControl } from './segmentedControl';
 export { Selection } from './selection';
 export { SpinLoader } from './spinLoader';
 export { SystemAlert } from './systemAlert';
