a0d929e52d70f4e3b3e66e60733a3c2fe4ab6fb2
a-rudak-EIS <a-rudak-EIS@users.noreply.github.com>
2026-04-08 14:45:52 +0000

    EPMRPP-86399 || 86399 || Value is cut in Number field for "Percent" rule (#277)
    * feat(fieldNumber): add Safari clip fix for input width calculation
    
    * fix(fieldNumber): adjust input width calculation for better accuracy 06c1ddbcceb3d709890c804f9f9bd33399040e1b

--- components/fieldNumber/constants.d.ts
+++ components/fieldNumber/constants.d.ts
@@ -1,5 +1,6 @@
 import { KeyCodes } from '../../common/constants/keyCodes';
 
 export declare const DEFAULT_WIDTH_CH = 5;
-export declare const MAX_WIDTH_CH = 16;
+export declare const SAFARI_CLIP_FIX_CH = 0.4;
+export declare const MAX_WIDTH_CH: number;
 export declare const ALLOWED_KEY_CODES: KeyCodes[];
